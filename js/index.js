c9bfdcbe828819ba6a6c7c7df9a33565099f10db
= <=>
2021-02-27 09:16:38 -0800

    Removed timer module, added broswer timer functionality

--- js/index.js
+++ js/index.js
@@ -1,13 +1,80 @@
-import { Timer } from "./timer.js";
+// I'm going to define a class to handle our timer functions.
+// This will help keep our logic in one place, and keep the REST
+// of our code from becoming too "verbose".
+class Timer {
+  /**
+   * Make a new timer object.
+   * @param {string} type The type of timer to build initially.
+   * @returns {Object}
+   */
+  constructor(type = "pomodoro") {
+    this.timerElement = document.getElementById(type);
+    this.clock = document.getElementById("time");
+    this.actionElement = document.getElementById("action");
+    this.timer = this.timerElement.value;
+    this.text = this.timer <= 9 ? `0${this.timer}` : `${this.timer}`;
+  }
 
-const navlinks = document.querySelectorAll("nav > ul > li");
-const navBg = document.getElementById("bgindicator");
-const settingsContainer = document.getElementById("settingscontainer");
-const time = document.getElementById("time");
+  /**
+   * Reset the timer with it's current settings.
+   */
+  reset() {
+    this.stop();
+    this.timer = this.timerElement.value;
+    this.text = this.timer <= 9 ? `0${this.timer}` : `${this.timer}`;
+    this.actionElement.innerText = "start";
+    this.clock.innerText = `${this.text}:00`;
+  }
+
+  /**
+   * Select a timer type.
+   * @param {string} type
+   */
+  select(type) {
+    this.timerElement = document.getElementById(type);
+    this.reset();
+  }
+
+  /**
+   * Start running the timer
+   */
+  start() {
+    // Set the start time
+    this.timer = this.timerElement.value * 60;
+    let minutes = 0;
+    let seconds = 0;
+    // start an interval of 1 second to track time
+    this.interval = setInterval(() => {
+      minutes = parseInt(this.timer / 60, 10);
+      seconds = parseInt(this.timer % 60, 10);
+
+      minutes = minutes < 10 ? "0" + minutes : minutes;
+      seconds = seconds < 10 ? "0" + seconds : seconds;
+
+      // Set the clock display.
+      this.clock.innerText = minutes + ":" + seconds;
+
+      // Clear the interval when we're done!
+      if (--this.timer < 0) {
+        this.timer = 0;
+        clearInterval(this.interval);
+        this.actionElement.innerText = "reset";
+      }
+    }, 1000);
+    this.actionElement.innerText = "stop";
+  }
+
+  stop() {
+    clearInterval(this.interval);
+    this.actionElement.innerText = "reset";
+  }
+}
 
 // ---------------------------------------------------------------------------
 // Navigation
 // ---------------------------------------------------------------------------
+const navlinks = document.querySelectorAll("nav > ul > li");
+const navBg = document.getElementById("bgindicator");
 
 // We need to make two loops.
 // The first loop cycles through all of the available nav links
@@ -33,9 +100,24 @@ navlinks.forEach((nav, i) =>
 
 const timer = new Timer();
 timer.reset();
+
+const action = (action = "stop") => {
+  switch (action.toLowerCase()) {
+    case "start":
+      timer.start();
+      break;
+    case "reset":
+      timer.reset();
+    default:
+      timer.stop();
+      break;
+  }
+};
+
 // ---------------------------------------------------------------------------
 // Settings Gear
 // ---------------------------------------------------------------------------
+const settingsContainer = document.getElementById("settingscontainer");
 
 // Click to open the settings window
 document
@@ -78,6 +160,3 @@ const inc = (input) => document.getElementById(input).stepUp(1);
  */
 
 const dec = (input) => document.getElementById(input).stepDown(1);
-
-timer.select("shortbreak");
-timer.start();
