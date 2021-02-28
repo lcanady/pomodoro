41526019cdda8f3adb97fd66eed8ae7b51a04734
= <=>
2021-02-28 06:12:12 -0800

    Progress on the settings window. Pomodoro timer settings CSS/HTML work.

--- js/index.js
+++ js/index.js
@@ -174,3 +174,8 @@ const inc = (input) => document.getElementById(input).stepUp(1);
  */
 
 const dec = (input) => document.getElementById(input).stepDown(1);
+
+document.getElementById("apply").addEventListener("click", (ev) => {
+  ev.preventDefault();
+  settingsContainer.style.display = "none";
+});
